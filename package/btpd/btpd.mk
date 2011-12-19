5e4bc97495c9606a52ea8c980a7b5d17b5546c10
Ciprian Badescu <cbadescu@x200debian.lan>
2011-12-19 23:25:36 +0200

    added btpd package

---
+++ package/btpd/btpd.mk
@@ -0,0 +1,17 @@
+#############################################################
+#
+# btpd
+#
+#############################################################
+BTPD_VERSION = 0.16
+BTPD_SOURCE = btpd-$(BTPD_VERSION).tar.gz
+BTPD_SITE = https://github.com/downloads/btpd/btpd
+BTPD_INSTALL_STAGING = NO
+BTPD_INSTALL_TARGET = YES
+BTPD_CONF_ENV += CFLAGS+=-I$(TARGET_DIR)/usr/include LDFLAGS+=-L$(TARGET_DIR)/usr/lib
+#BTPD_CONF_ENV = CFLAGS=-I$(TARGET_DIR)/usr/include LDFLAGS="$(TARGET_LDFLAGS)"
+#BTPD_CONF_OPT =
+BTPD_DEPENDENCIES = openssl
+
+$(eval $(call AUTOTARGETS,package,libfoo))
+
