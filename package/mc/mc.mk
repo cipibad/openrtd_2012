2a138a562bc4b3a64cab942a7b73059200a9cbb0
Ciprian Badescu <cbadescu@x200debian.lan>
2011-12-28 21:34:20 +0200

    clean compile

--- package/mc/mc.mk
+++ package/mc/mc.mk
@@ -6,12 +6,13 @@
 MC_VERSION = 4.6.1
 MC_SOURCE = mc-$(MC_VERSION).tar.gz
 MC_SITE = http://ftp.gnu.org/gnu/mc
-MC_INSTALL_STAGING = NO
+MC_INSTALL_STAGING = YES
 MC_INSTALL_TARGET = YES
-MC_CONF_ENV += LDFLAGS+=-L$(TARGET_DIR)/usr/lib
+MC_CONF_OPT = --with-vfs=no --with-x=no --with-gpm-mouse=no --with-screen=mcslang
+MC_CONF_ENV += LDFLAGS+="-L$(TARGET_DIR)/usr/lib -liconv"
 
-MC_DEPENDENCIES = ncurses
+MC_DEPENDENCIES = slang ncurses host-mc
 
-
-$(eval $(call AUTOTARGETS,package,mc))
+$(eval $(call AUTOTARGETS,host))
+$(eval $(call AUTOTARGETS))
 
