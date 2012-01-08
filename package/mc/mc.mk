0bc57d2ae6bff9fc14421e406861a7076e77fb20
Ciprian Badescu <cbadescu@x200debian.lan>
2012-01-08 12:59:11 +0200

    modification to get mc

--- package/mc/mc.mk
+++ package/mc/mc.mk
@@ -9,9 +9,10 @@ MC_SITE = http://ftp.gnu.org/gnu/mc
 MC_INSTALL_STAGING = YES
 MC_INSTALL_TARGET = YES
 MC_CONF_OPT = --with-vfs=no --with-x=no --with-gpm-mouse=no --with-screen=mcslang
-MC_CONF_ENV += LDFLAGS+="-L$(TARGET_DIR)/usr/lib -liconv"
+MC_CONF_ENV = LDFLAGS+="-L$(TARGET_DIR)/usr/lib -liconv"
+HOST_MC_CONF_ENV = 
 
-MC_DEPENDENCIES = slang ncurses host-mc
+MC_DEPENDENCIES = slang ncurses host-libiconv host-mc 
 
 $(eval $(call AUTOTARGETS,host))
 $(eval $(call AUTOTARGETS))
